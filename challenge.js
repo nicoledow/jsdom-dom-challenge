5dd36661ea95aef8213a9f00e2935922d54f6880
Nicole Dow <nikki.dow71@gmail.com>
2019-10-15 15:44:17 -0400

    Done.

--- challenge.js
+++ challenge.js
@@ -3,13 +3,17 @@ const minusButton = document.getElementById('minus');
 const heartButton = document.getElementById('heart');
 let counter = document.getElementById('counter');
 let currentCount = parseInt(counter.innerText, 10);
+let counting = true;
 const likes = document.querySelector('p.likes');
 const likesCounts = {}
+const pauseButton = document.getElementById('pause');
 
 function count() {
+    if (counting === true) {
     currentCount += 1
     counter.innerText = `${currentCount}`
     likes.innerText = `${likesCounts[currentCount] || 0} likes`
+    }
   }
 
 function addLike(num) {
@@ -20,6 +24,21 @@ function addLike(num) {
     }
 }
 
+function pause() {
+    if (pauseButton.innerText === "pause") {
+        pauseButton.innerText = "unpause";
+        counting = false;
+        plusButton.disabled = true;
+        minusButton.disabled = true;
+        heartButton.disabled = true;
+    } else {
+        pauseButton.innerText = "pause"
+        counting = true;
+        plusButton.disabled = false;
+        minusButton.disabled = false;
+        heartButton.disabled = false;
+    }
+}
 
 plusButton.addEventListener('click', () => {
   currentCount += 1;
@@ -31,6 +50,7 @@ minusButton.addEventListener('click', () => {
 
 heartButton.addEventListener('click', addLike(currentCount));
 
+pauseButton.addEventListener('click', pause);
 
 document.addEventListener("DOMContentLoaded", () => {
     window.setInterval(count, 1000);
